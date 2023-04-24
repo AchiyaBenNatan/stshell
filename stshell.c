db5c00b915ad3f5525dabe1a3ba789ce102c5dc4
AchiyaBenNatan <120559596+AchiyaBenNatan@users.noreply.github.com>
2023-04-24 13:21:23 +0300

    Update stshell.c

--- stshell.c
+++ stshell.c
@@ -11,18 +11,6 @@
 
 void execute_command(char *args[MAX_ARGS], int input_fd, int output_fd)
 {
-    //Handle built-in commands
-    // if (strcmp(args[0], "cd") == 0) {
-    //     if (args[1] == NULL) {
-    //         // No argument provided to cd, go to home directory
-    //         chdir(getenv("HOME"));
-    //     } else {
-    //         if (chdir(args[1]) != 0) {
-    //             fprintf(stderr, "cd: %s: No such file or directory\n", args[1]);
-    //         }
-    //     }
-    //     return;
-    // }
     // Fork to execute command
     pid_t pid = fork();
 
@@ -235,9 +223,3 @@ int main()
 
     return 0;
 }
-// vboxuser@Achiya:~/Desktop/stshell$ make
-// make: Nothing to be done for 'all'.
-// vboxuser@Achiya:~/Desktop/stshell$ ./stshell
-// stshell> cat check.txt | sort | uniq > cmp_sort_uniq.txt
-// sort: read failed: -: Bad file descriptor
-// uniq: '>': No such file or directory
